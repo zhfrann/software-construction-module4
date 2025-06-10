97310138b32e010fc9e1ffaada3a4c1204055a36
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-06-10 12:53:13 +0700

    menambahkan class KodeProduk

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System.Reflection;
+
+class KodeProduk
+{
+    private string[] namaProduk = { "Laptop", "Smarthphone", "Tablet", "Headset", "Keyboard", "Mouse", "Printer", "Monitor", "Smartwatch", "Kamera" };
+    private string[] kodeProduk = { "E100", "E101", "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109" };
+
+    public string getKodeProduk(string namaProduk)
+    {
+        int idxProduk = Array.IndexOf(this.namaProduk, namaProduk);
+        return kodeProduk[idxProduk];
+    }
+}
